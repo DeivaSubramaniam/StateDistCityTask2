a2dc24c2046e7ebad98a64274879e1b35ff71f00
DeivaSubramaniam <deivamanimca@gmail.com>
2017-12-05 12:22:47 +0530

    Without Unit Test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
 import { AppComponent } from './app.component';
-
-
+import { CityComponent } from './city/city.component';
+import { DistrictComponent } from './district/district.component';
+import { StateComponent } from './state/state.component';
+import { DataService } from './shared/data.service';
+import { FormsModule } from '@angular/forms';
+import * as _ from 'lodash';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CityComponent,
+    DistrictComponent,
+    StateComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+FormsModule
   ],
-  providers: [],
+  providers: [DataService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
